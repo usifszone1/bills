ea1e99c84fd4fa6d575eff187287d1cfb6a50ae6
usifszone1 <joe_khaled@ymail.com>
2025-03-11 08:16:43 +0200

    Update HistoryView.tsx

--- src/components/Views/HistoryView.tsx
+++ src/components/Views/HistoryView.tsx
@@ -1,1 +1,27 @@
 
+import React from "react";
+import InvoiceHistory from "../InvoiceHistory";
+import { Receipt as ReceiptType } from "../../utils/sampleData";
+
+interface HistoryViewProps {
+  onSelectReceipt: (receipt: ReceiptType) => void;
+  onSelectMultipleReceipts: (receipts: ReceiptType[]) => void;
+}
+
+const HistoryView: React.FC<HistoryViewProps> = ({
+  onSelectReceipt,
+  onSelectMultipleReceipts,
+}) => {
+  return (
+    <div className="min-h-[60vh] py-8">
+      <div className="container max-w-5xl mx-auto px-4">
+        <InvoiceHistory 
+          onSelectReceipt={onSelectReceipt}
+          onSelectMultipleReceipts={onSelectMultipleReceipts}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HistoryView;
